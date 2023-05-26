cfe3806cbef1cebaf282087bda7de0ad25b94435
Vaiyolent <danuarta70@gmail.com>
2023-05-26 15:38:01 +0700

    update

--- resources/views/frontend/include/navbar.blade.php
+++ resources/views/frontend/include/navbar.blade.php
@@ -1,12 +1,13 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3 bg-dark">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-md-3">
     <div class="container">
         <img src="{{ asset('assets/frontend/css/images/RawalaText.png') }}" alt="Logo" width="100" class="d-inline-block align-text-top" href="#">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav ms-auto">
-          @guest
+            @guest
             <li class="nav-item">
               <a href="{{ url('/') }}" class="nav-link {{ request()->is('/') ? ' active-link' : '' }}">
                   <span>Home</span>
